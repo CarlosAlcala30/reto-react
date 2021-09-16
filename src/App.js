87b67fa031de0f2be066e051d81a5fe2da798bfd
carlosalcala30 <calcala@bizprobpm.ca>
2021-09-16 10:30:10 -0500

    proyecto base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+            <li>
+              <Link to="/create-post">Create Post</Link>
+            </li>
+            <li>
+              <Link to="/search">Search</Link>
+            </li>
+            <li>
+              <Link to="/post-detail/4wda1sda4f">Post Detail</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/create-post">
+            <CreatePost />
+          </Route>           
+          <Route path="/post-detail/:id" >
+            <PostDetail />
+          </Route>   
+          <Route path="/">
+            <Home />
+          </Route>                   
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Login() {
+  return <h2>Login</h2>;
+}
+
+function Register() {
+  return <h2>Register</h2>;
+}
+
+function Search() {
+  return <h2>Search</h2>;
+}
+
+function CreatePost() {
+  return <h2>CreatePost</h2>;
+}
+
+function PostDetail() {
+  const {id} = useParams();
+  return <h2>Post Detail - {id}</h2>;
+}
+
+
