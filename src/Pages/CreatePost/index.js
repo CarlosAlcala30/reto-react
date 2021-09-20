a90c8df14bead340ae9824766b6adbb685e14480
carlosalcala30 <calcala@bizprobpm.ca>
2021-09-19 23:40:43 -0500

    cover create post

--- src/Pages/CreatePost/index.js
+++ src/Pages/CreatePost/index.js
@@ -1,11 +1,57 @@
-import React from "react";
+import React,{useState} from "react";
+
 
 import "./style.scss";
 
-const CreatePostPage = (props) => {  
-    return (
-        <h2>Create-post</h2>
-    )
-}
+import FilterLink from "../../Components/Links/FilterLink";
+
+const CreatePostPage = (props) => {
+
+    const [coverImage,setCoverImage]=useState();
+
+    const handlerImg = (event) => {
+        event.preventDefault();
+        setCoverImage({file:URL.createObjectURL(event.target.files[0])})
+    }
+
+  return (
+    <main className="main">
+      <div className="header-container">
+        <div>
+          <h4>Create Post</h4>
+        </div>
+        <div>
+        <ul>
+          <FilterLink text="Edit" />
+          <FilterLink text="Preview" />
+        </ul>
+        </div>
+      </div>
+      <form className="form-container">        
+            {
+                coverImage ? 
+                (
+                    <div class="cover">
+                        <img src={coverImage.file} alt="" />
+                        <label for="cover" className="btn-add">Change</label>
+                        <input type="file" id="cover" onChange={handlerImg}></input>
+                        <button type="button" className="btn-remove" onClick={ () => { setCoverImage()} }>Remove</button>
+                    </div>
+                ) 
+                    : 
+                (
+                    <div class="cover">
+                        <label for="cover" className="btn-add">Add a cover image</label>
+                        <input type="file" id="cover" onChange={handlerImg}></input>
+                    </div>
+                    
+                ) 
+            }          
+        
+
+      </form>
+    </main>
+  );
+};
 
 export default CreatePostPage;
